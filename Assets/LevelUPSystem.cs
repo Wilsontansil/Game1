dcfc44b8c7d371d37aaea5f12697755a6213da8d
wilsonTansil <tanwilson2410@gmail.com>
2020-05-10 17:05:01 +0700

    Fix

---
+++ Assets/LevelUPSystem.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class LevelUPSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref PlayerLevelComponentData PLC) =>
+        {
+                PLC.playerLevel += 1;
+        }).ScheduleParallel();
+    }
+}
